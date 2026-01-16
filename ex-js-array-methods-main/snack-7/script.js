c7ed0150e2ec68eea8e7121fa7661bda1821529e
NoemiMassucco <noemimassucco@gmail.com>
2026-01-16 22:21:57 +0100

    Eseguito esercizio 7

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -20,4 +20,6 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+const id2 = students.find(student => student.id === 2 );
+console.log(id2);
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
